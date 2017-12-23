29d642885bc48aeecb4f4a9f928340203014c143
John McCarthy <midactsmystery@gmail.com>
2017-12-23 18:32:14 -0500

    adding xdotool and libpcre16.so.0 symlink

--- tibiaclientupdate.sh
+++ tibiaclientupdate.sh
@@ -4,6 +4,9 @@ user=$(whoami)
 # Changes to the desktop
 cd /home/$user/Desktop
 
+# Makes the Tibia directory only if it does not exist
+mkdir -p Tibia
+
 # Gets the latest Tibia version
 wget http://download.tibia.com/tibia.x64.tar.gz
 
@@ -20,3 +23,9 @@ cp -r Tibia-Latest/$Latest/* Tibia/
 # Deletes the tar and temp new directory
 rm -rf Tibia-Latest
 rm -rf tibia.x64.tar.gz*
+
+# Makes sure the libpcre16.0.3 exists
+ln -s /usr/lib/x86_64-linux-gnu/libpcre16.so.3 /home/$user/Desktop/Tibia/bin/libpcre16.so.0
+
+# Makes sure xdotool is installed
+apt-get install -y xdotool
